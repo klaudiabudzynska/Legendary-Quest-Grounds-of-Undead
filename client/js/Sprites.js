cd16dff76bcc90e23ec0bbd36239daaa06dc4bfb
Klaudia Budzyńska <klaudia.budzynska1@gmail.com>
2019-12-28 15:42:58 +0100

    Refractoring

--- client/js/Sprites.js
+++ client/js/Sprites.js
@@ -1,20 +1,18 @@
 export default class Sprites {
 
-  constructor(url, ctx, size){
+  constructor(url, size){
     this.url = url;
-    this.ctx = ctx;
     this.size = size;
-    this.defaultSize = 16;
+    this.defaultSize = 32;
     this.tiles = new Map();
   }
 
   define(name, x, y){
     const buffer = document.createElement('canvas');
-    buffer.width = this.size;
-    buffer.height = this.size;
-    buffer
-      .getContext('2d')
-      .drawImage(
+    buffer.width = this.defaultSize;
+    buffer.height = this.defaultSize;
+    const bufferCtx = buffer.getContext('2d');
+    bufferCtx.drawImage(
         this.url,
         x * this.size,
         y * this.size,
@@ -23,12 +21,14 @@ export default class Sprites {
         0, 0,
         this.defaultSize,
         this.defaultSize
-      );
+    );
+    bufferCtx.rect(0, 0, buffer.width, buffer.height);
+    bufferCtx.stroke();
     this.tiles.set(name, buffer);  
   }
 
-  draw(name, x, y){
+  draw(name, ctx, x, y){
     const buffer = this.tiles.get(name);
-    this.ctx.drawImage(buffer, x * this.defaultSize, y * this.defaultSize);
+    ctx.drawImage(buffer, x * this.defaultSize, y * this.defaultSize);
   }
 }
--- client/js/index.js
+++ client/js/index.js
@@ -1,31 +1,9 @@
-import { imageLoader } from './loaders.js';
-import Sprites from './Sprites.js';
+import { loadBackground, loadCharacter } from './spriteSheet.js';
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 ctx.scale(3,3);
-
-function loadBackground(){
-  return imageLoader('../img/TilesetGraveyard.png')
-  .then(img => {
-    console.log('Tileset loaded ', img);
-    const background = new Sprites(img, ctx, 16);
-    background.define('rock-1', 0, 0);
-    return background;
-    
-  })
-}
-
-function loadCharacter(){
-  return imageLoader('../img/Human/human_regular_hair.png')
-  .then( img => {
-    console.log('Character loaded', img);
-    const human = new Sprites(img, ctx, 20);
-    human.define('idle', 1, 0);
-    return human;
-  })
-}
 
 Promise.all([
   loadBackground(), 
@@ -37,9 +15,6 @@ Promise.all([
 ]) => {
   ctx.fillStyle = "#89d27c";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  background.draw('rock-1', 0, 0);
-  character.draw('idle', 1, 0);
+  background.draw('rock-1', ctx, 0, 0);
+  character.draw('idle', ctx, 1, 0);
 })
-
-
-
